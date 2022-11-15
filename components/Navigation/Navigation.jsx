6cf056e695d44657f583b87bd4b691492aa6cd8b
Adrian Gąsiorek <adriangasiorek@MacBook-Pro-Adrian.local>
2022-11-15 22:59:16 +0100

    logo redirects to home

--- components/Navigation/Navigation.jsx
+++ components/Navigation/Navigation.jsx
@@ -2,18 +2,28 @@ import styled from 'styled-components'
 import { NavigationLink } from '../Link'
 import React from 'react'
 import Center from 'layouts/Center'
+import Link from '../Link/Link'
+
 
 const StyledNav = styled.nav`
   width: 100%;
   background-color: ${({ theme }) => theme.color.navBackground};
   border-bottom: 2px solid ${({ theme }) => theme.color.accent};
 `
-
 const StyledLogo = styled.img`
   height: 100%;
   align-self: flex-start;
   color:${({ theme }) => theme.color.accent};
 `
+const StyledLogoLink = styled(Link)`
+`
+
+const LogoLink = ({href, src}) =>(
+    <StyledLogoLink href={href}>
+      <StyledLogo src={src} />
+    </StyledLogoLink>
+
+)
 const StyledOrderedList = styled.ol`
   margin-left: auto;
 `
@@ -22,7 +32,7 @@ const Navigation = ({ children }) => (
   <>
     <StyledNav>
       <Center intrinsicCenter>
-        <StyledLogo src="/static/images/logo.svg" alt="LA1TV"/>
+        <LogoLink href="/" src="/static/images/logo.svg"/>
         <StyledOrderedList>
           <NavigationLink href="/">Home</NavigationLink>
           <NavigationLink href="/browse/latest">Latest</NavigationLink>
